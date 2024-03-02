ccc91bc5466d9ca5f489daf9cef049ef7c462ad8
LuminousBinary <boolwrld@gmail.com>
2024-03-02 14:20:41 +0000

    removeed .env req

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 // })
 
 // if (process.env.NODE_ENV !== "production") {
-require("dotenv").config();
+// require("dotenv").config();
 // }
 const express = require("express");
 const mongoose = require("mongoose");
